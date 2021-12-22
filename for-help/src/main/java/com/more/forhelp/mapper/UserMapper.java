e790d6c6820332916065b116919ec1a95f285ec0
liudehao <aaa>
2021-12-23 00:25:24 +0800

    test

---
+++ for-help/src/main/java/com/more/forhelp/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.more.forhelp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.more.forhelp.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
